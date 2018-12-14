f28d36d834b14b7ed1908d5f9a0f0794ead75bc0
Samuel Torres <torressam333@gmail.com>
2018-12-13 20:28:35 -0700

    Corrections made to config

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return [
     'database' => [
         'name' => 'mytodo',
-        'username' => 'root',
-        'password' => 'Webdev1!',
+        'username' => '',
+        'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
